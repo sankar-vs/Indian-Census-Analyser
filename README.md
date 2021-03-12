d186cf43195686b478d48b2ca91b68c86c21fe7f
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-03-12 15:27:52 +0530

    [Sankar]ADDED: Happy test case where records are checked

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ## Welcome to Indian Census Analyser Program
 ### Used Case:1
 #### Ability for the analyser to load the Indian States Census Information from a CSV
+##### 1.1 Given the States Census CSV file, Check to ensure the Number of Record matches
---
+++ src/test/java/javapractice/CensusAnalyserTest.java
@@ -0,0 +1,20 @@
+package javapractice;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CensusAnalyserTest {
+    private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
+    private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
+
+    @Test
+    public void givenIndianCensusCSVFileReturnsCorrectRecords() {
+        try {
+            IndianCensusAnalyser censusAnalyser = new IndianCensusAnalyser();
+            int numOfRecords = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
+            Assertions.assertEquals(29,numOfRecords);
+        } catch (CensusAnalyserException e) {
+            System.out.println(e.toString());
+        }
+    }
+}
