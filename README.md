7e435f8c4cdd69d18d1a6901b2f76fe155f7586b
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-03-12 15:30:29 +0530

    [Sankar]ADDED: Sad Test Case to verify if the exception is raised

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 ### Used Case:1
 #### Ability for the analyser to load the Indian States Census Information from a CSV
 ##### 1.1 Given the States Census CSV file, Check to ensure the Number of Record matches
+##### 1.2 Given the State Census CSV File if incorrect Returns a custom Exception
--- src/test/java/javapractice/CensusAnalyserTest.java
+++ src/test/java/javapractice/CensusAnalyserTest.java
@@ -2,6 +2,7 @@ package javapractice;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
 
 public class CensusAnalyserTest {
     private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
@@ -15,6 +16,18 @@ public class CensusAnalyserTest {
             Assertions.assertEquals(29,numOfRecords);
         } catch (CensusAnalyserException e) {
             System.out.println(e.toString());
+        }
+    }
+
+    @Test
+    public void givenIndiaCensusData_WithWrongFile_ShouldThrowException() {
+        try {
+            IndianCensusAnalyser censusAnalyser = new IndianCensusAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            censusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_PATH);
+        } catch (CensusAnalyserException e) {
+            Assertions.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
         }
     }
 }
